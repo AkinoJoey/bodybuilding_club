d8b01a114e95c0fddaf5517907e8ed0cd91be326
AkinoJoey <gakiya213@gmail.com>
2023-06-22 17:48:03 +0900

    create up to external link

--- bodybuilding_club/members/urls.py
+++ bodybuilding_club/members/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
+    path('', views.index, name="index"),
     path('rules/', views.rules, name="rules"),
-    path('members/', views.members, name="members"),
-    path('members/details/<int:id>', views.details, name="details"),
+    path('notables/', views.notablesList, name="notables"),
+    path('notables/<int:notablesIndex>', views.notablesDetail, name="notablesDetail"),
+    path('externalLinks/', views.externalLinks, name="externalLinks"),
     path('testing/', views.testing, name='testing'),
 ]
--- bodybuilding_club/members/views.py
+++ bodybuilding_club/members/views.py
@@ -1,29 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 import random    
-from django.views.generic import TemplateView
 from .models import Member
-class IndexView(TemplateView):
-    template_name = 'index.html'
-    
-    def get_context_data(self):
-        image_list = ['image1.webp', 'image2.webp', 'image3.webp']
-        random_image = image_list[random.randint(0, len(image_list)-1)]
-        context = {
-            "random_image": f"members/images/index_background/{random_image}"
-        
-        }
-        
-        return context
 
-def members(request):
+def index(request):
+    image_list = ['image1.webp', 'image2.webp', 'image3.webp']
+    random_image = image_list[random.randint(0, len(image_list)-1)]
+    context = {
+        "random_image": f"members/images/index_background/{random_image}"
+    
+    }
+    
+    return render(request,'index.html', context)
+
+def notablesList(request):
     members = Member.objects.all().values()
     context = {
         'members':  members
     }
     return render(request, 'all_members.html', context)
 
-def details(request, id):
-    member = Member.objects.get(id=id)
+def notablesDetail(request, notablesIndex):
+    member = Member.objects.get(id=notablesIndex)
     context = {
         'member': member
     }
@@ -33,5 +30,12 @@ def details(request, id):
 def rules(request):
     return render(request, 'rules.html')
 
+def externalLinks(request):
+    link_list = ["https://en.wikipedia.org/wiki/Arnold_Schwarzenegger","https://en.wikipedia.org/wiki/Ronnie_Coleman","https://en.wikipedia.org/wiki/Lee_Haney"]
+    context = {
+        "link_list": link_list
+    }
+    
+    return render(request, 'externalLinks.html', context)
 def testing(request):
     return render(request, 'base.html')
